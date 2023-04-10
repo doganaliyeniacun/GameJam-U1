804c98a183e278d650379c2f52df92204c6204ce
Doğan Ali Yeniacun <alidoganyeniacun@gmail.com>
2023-04-10 03:26:25 +0300

    changes font

---
+++ Assets/FinishArea.cs
@@ -0,0 +1,38 @@
+
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FinishArea : MonoBehaviour
+{
+    [SerializeField] private GameObject finishScene;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player") && finishScene)
+        {
+            finishScene.SetActive(true);
+            MoveImg();
+        }
+    }
+
+    private void Update()
+    {
+        if (finishScene != null && Input.GetMouseButtonDown(0))
+        {
+            HideImgAndRestarGame();
+        }
+    }
+
+    public void HideImgAndRestarGame()
+    {
+        finishScene.SetActive(false);
+        SceneManager.LoadScene(1);
+    }
+
+    public void MoveImg()
+    {
+        Transform transformImg = finishScene.transform.GetChild(0).transform.GetChild(0).transform;
+        transformImg.DOMoveY(transformImg.position.y - 200, 60);
+    }
+}
